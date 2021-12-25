a8b58518d85b285fa90d818cf40038def0a81ce2
harry_6.00 <77897280+prabudh12@users.noreply.github.com>
2021-12-25 14:28:51 +0530

    Create InsertAtAGivenPosition.cpp

---
+++ Linked List/InsertAtAGivenPosition.cpp
@@ -0,0 +1,19 @@
+Node insertPos(Node *head, int pos, int data)
+{
+  Node *temp = new Node(data);
+  if(pos == 1)
+  {
+    temp->next = head;
+   return temp;
+  }
+  Node *curr = head;
+  for(int i = 1; i<=pos-2 && curr!=NULL; i++)
+    curr = curr->next;
+  
+  if(curr == NULL)
+    return head;
+  
+  temp->next = curr->next;
+  curr->next = temp;
+  return head;
+  
