f33dc1bdaf59c8820b91b140637a1ce5209606c2
harry_6.00 <77897280+prabudh12@users.noreply.github.com>
2021-12-25 14:37:52 +0530

    Update Search(Recursive).cpp

--- Linked List/Search(Recursive).cpp
+++ Linked List/Search(Recursive).cpp
@@ -19,13 +19,17 @@ void printlist(Node *head){
 }
 
 int search(Node * head, int x){
-    if(head==NULL)return -1;
-    if(head->data==x)return 1;
-    else{
-        int res=search(head->next,x);
-        if(res==-1)return -1;
-        else return res+1;
+    int pos=1;
+    Node *curr=head;
+    while(curr!=NULL){
+        if(curr->data==x)
+            return pos;
+        else{
+            pos++;
+            curr=curr->next;
+        }
     }
+    return -1;
 }
 
 int main() 
