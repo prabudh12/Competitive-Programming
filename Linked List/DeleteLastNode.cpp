45fb8fe696b4754accdd72da025279e9ae99e6cc
harry_6.00 <77897280+prabudh12@users.noreply.github.com>
2021-12-25 14:30:09 +0530

    Update DeleteLastNode.cpp

--- Linked List/DeleteLastNode.cpp
+++ Linked List/DeleteLastNode.cpp
@@ -1,66 +1,44 @@
-// CPP program to remove last node of
-// linked list.
-#include <iostream>
-using namespace std;
+#include <bits/stdc++.h> 
+using namespace std; 
 
-/* Link list node */
-struct Node {
-	int data;
-	struct Node* next;
+struct Node{
+    int data;
+    Node* next;
+    Node(int x){
+        data=x;
+        next=NULL;
+    }
 };
 
-/* Function to remove the last node
-of the linked list */
-Node* removeLastNode(struct Node* head)
-{
-	if (head == NULL)
-		return NULL;
-
-	if (head->next == NULL) {
-		delete head;
-		return NULL;
-	}
-
-	// Find the second last node
-	Node* second_last = head;
-	while (second_last->next->next != NULL)
-		second_last = second_last->next;
-
-	// Delete last node
-	delete (second_last->next);
-
-	// Change next of second last
-	second_last->next = NULL;
-
-	return head;
+void printlist(Node *head){
+    Node *curr=head;
+    while(curr!=NULL){
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }cout<<endl;
 }
 
-// Function to push node at head
-void push(struct Node** head_ref, int new_data)
-{
-	struct Node* new_node = new Node;
-	new_node->data = new_data;
-	new_node->next = (*head_ref);
-	(*head_ref) = new_node;
+Node *delTail(Node *head){
+    if(head==NULL)return NULL;
+    if(head->next==NULL){
+        delete head;
+        return NULL;
+    }
+    Node *curr=head;
+    while(curr->next->next!=NULL)
+        curr=curr->next;
+    delete (curr->next);
+    curr->next=NULL;
+    return head;
 }
-
-// Driver code
-int main()
-{
-	/* Start with the empty list */
-	Node* head = NULL;
-
-	/* Use push() function to construct
-	the below list 8 -> 23 -> 11 -> 29 -> 12 */
-	push(&head, 12);
-	push(&head, 29);
-	push(&head, 11);
-	push(&head, 23);
-	push(&head, 8);
-
-	head = removeLastNode(head);
-	for (Node* temp = head; temp != NULL; temp = temp->next)
-		cout << temp->data << " ";
-
+int main() 
+{ 
+	Node *head=new Node(10);
+	head->next=new Node(20);
+	head->next->next=new Node(30);
+	printlist(head);
+	head=delTail(head);
+	printlist(head);
+	
 	return 0;
-}
+} 
